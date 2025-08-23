d11fcb55b0ac1b247d1c32abd110d7bf4f3cc8df
MonicaKennedy92 <monicakennedyofficial@gmail.com>
2025-08-23 11:11:31 -0600

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Monica Kennedy on 2025-08-23.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
